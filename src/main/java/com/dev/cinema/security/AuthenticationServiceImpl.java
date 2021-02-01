1419bc0e9328916323b387349a98d141ad3112c6
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-01 10:53:20 +0200

    added AuthenticationService and PasswordHashing

---
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.dev.cinema.security;
+
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.UserService;
+
+import javax.naming.AuthenticationException;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userByEmail = userService.findByEmail(email);
+        String hash = PasswordHashing.getHash(password, userByEmail.get().getSalt());
+        if (userByEmail.get().getPassword().equals(hash)) {
+            return userByEmail.get();
+        }
+        throw new AuthenticationException("Incorrect password");
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        byte[] salt = PasswordHashing.getSalt();
+        user.setPassword(PasswordHashing.getHash(password, salt));
+        user.setSalt(salt);
+        return userService.add(user);
+    }
+}
---
+++ src/main/java/com/dev/cinema/security/PasswordHashing.java
@@ -0,0 +1,30 @@
+package com.dev.cinema.security;
+
+import com.dev.cinema.exceptions.DataProcessingException;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+public class PasswordHashing {
+    public static String getHash (String password, byte[] salt) {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        try {
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = secretKeyFactory.generateSecret(spec).getEncoded();
+            return Arrays.toString(hash);
+        } catch (GeneralSecurityException e) {
+            throw new DataProcessingException("Cannot hashing password " + password, e);
+        }
+    }
+
+    public static byte[] getSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+}

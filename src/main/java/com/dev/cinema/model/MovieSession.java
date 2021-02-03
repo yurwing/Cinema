04b1bce0d104f3740816153d5c24998d0dea40d9
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-03 19:03:19 +0200

    checkstyle fixed

--- src/main/java/com/dev/cinema/model/MovieSession.java
+++ src/main/java/com/dev/cinema/model/MovieSession.java
@@ -3,7 +3,6 @@ package com.dev.cinema.model;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;

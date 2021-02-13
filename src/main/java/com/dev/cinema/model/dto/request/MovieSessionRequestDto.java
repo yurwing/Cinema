d31dc6b4e9c1bd44c94123e920f98b31f2a4bb7e
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-13 22:09:52 +0200

    created response and request DTO

---
+++ src/main/java/com/dev/cinema/model/dto/request/MovieSessionRequestDto.java
@@ -0,0 +1,40 @@
+package com.dev.cinema.model.dto.request;
+
+public class MovieSessionRequestDto {
+    private Long id;
+    private Long movieId;
+    private Long cinemaHallId;
+    private String showTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaHallId() {
+        return cinemaHallId;
+    }
+
+    public void setCinemaHallId(Long cinemaHallId) {
+        this.cinemaHallId = cinemaHallId;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+}

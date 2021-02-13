a6ddc0d1d258ad881ef9188de49803dcdded74f7
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-13 22:11:29 +0200

    implemented controllers

---
+++ src/main/java/com/dev/cinema/controller/MovieSessionController.java
@@ -0,0 +1,56 @@
+package com.dev.cinema.controller;
+
+import com.dev.cinema.model.dto.request.MovieSessionRequestDto;
+import com.dev.cinema.model.dto.response.MovieSessionResponseDto;
+import com.dev.cinema.service.MovieSessionService;
+import com.dev.cinema.service.mapper.MovieSessionMapper;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie-sessions")
+public class MovieSessionController {
+    private final MovieSessionService movieSessionService;
+    private final MovieSessionMapper movieSessionMapper;
+
+    public MovieSessionController(MovieSessionService movieSessionService,
+                                  MovieSessionMapper movieSessionMapper) {
+        this.movieSessionService = movieSessionService;
+        this.movieSessionMapper = movieSessionMapper;
+    }
+
+    @GetMapping("/available")
+    public List<MovieSessionResponseDto> getAllAvailable(@RequestParam Long movieId,
+                                                         @RequestParam
+                                                         @DateTimeFormat(pattern = "dd.MM.yyyy")
+                                                                 LocalDate date) {
+        return movieSessionService.findAvailableSessions(movieId, date).stream()
+                .map(movieSessionMapper::getDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void addMovieSession(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        movieSessionService.add(movieSessionMapper.getEntity(movieSessionRequestDto));
+    }
+
+    @DeleteMapping
+    public void deleteMovieSession(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        movieSessionService.delete(movieSessionMapper.getEntity(movieSessionRequestDto));
+    }
+
+    @PutMapping
+    public void updateMovieSession(@RequestBody MovieSessionRequestDto movieSessionRequestDto) {
+        movieSessionService.update(movieSessionMapper.getEntity(movieSessionRequestDto));
+    }
+}

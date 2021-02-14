78f9803597cd29501ae3f0f09866090b3bab0a9a
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-14 16:15:15 +0200

    created dto dor User, ShoppingCart and Order

---
+++ src/main/java/com/dev/cinema/model/dto/request/ShoppingCartRequestDto.java
@@ -0,0 +1,22 @@
+package com.dev.cinema.model.dto.request;
+
+public class ShoppingCartRequestDto {
+    private String userEmail;
+    private Long movieSessionId;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Long getMovieSessionId() {
+        return movieSessionId;
+    }
+
+    public void setMovieSessionId(Long movieSessionId) {
+        this.movieSessionId = movieSessionId;
+    }
+}

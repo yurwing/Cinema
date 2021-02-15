47b1e01c88d531293d5af3f7e1de415620257713
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-15 23:00:29 +0200

    implemented SecurityConfig

--- src/main/java/com/dev/cinema/config/SecurityConfig.java
+++ src/main/java/com/dev/cinema/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.dev.cinema.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("1234").roles("USER");
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(getEncoder())
+                .withUser("user").password(getEncoder().encode("1234")).roles("USER");
     }
 
     @Override
@@ -23,6 +28,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .permitAll()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

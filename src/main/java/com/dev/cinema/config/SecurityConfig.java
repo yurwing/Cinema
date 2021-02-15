bef689e05eb90b294c38a02965ff06baf938ea92
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-15 17:04:49 +0200

    added config files and dependencies

---
+++ src/main/java/com/dev/cinema/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.dev.cinema.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("1234").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ src/main/java/com/dev/cinema/config/WebSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.dev.cinema.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}

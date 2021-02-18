63979544e0e1689d76c600eeca924764acbeed1f
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-18 16:00:02 +0200

    delete unnecessary code

--- src/main/java/com/dev/cinema/service/mapper/UserMapper.java
+++ src/main/java/com/dev/cinema/service/mapper/UserMapper.java
@@ -3,20 +3,11 @@ package com.dev.cinema.service.mapper;
 import com.dev.cinema.model.User;
 import com.dev.cinema.model.dto.request.UserRequestDto;
 import com.dev.cinema.model.dto.response.UserResponseDto;
-import com.dev.cinema.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper implements MapperToDto<User,
         UserResponseDto>, MapperToEntity<User, UserRequestDto> {
-    private final RoleService roleService;
-
-    @Autowired
-    public UserMapper(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
     @Override
     public UserResponseDto getDto(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();

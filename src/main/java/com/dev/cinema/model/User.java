856a4f99582a0f3823e53fce7d8d2ab95c6bb58b
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-21 21:33:31 +0200

    remade naming for Role

--- src/main/java/com/dev/cinema/model/User.java
+++ src/main/java/com/dev/cinema/model/User.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -19,6 +21,9 @@ public class User {
     private String email;
     private String password;
     @ManyToMany
+    @JoinTable(name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles;
 
     public Long getId() {

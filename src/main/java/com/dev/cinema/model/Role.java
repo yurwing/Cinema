cb5d4a2a8f4b23ea7b518255320c30a25b20a6b6
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-22 16:24:26 +0200

    made create-drop

--- src/main/java/com/dev/cinema/model/Role.java
+++ src/main/java/com/dev/cinema/model/Role.java
@@ -1,5 +1,6 @@
 package com.dev.cinema.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -15,6 +16,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
     private RoleName roleName;
 
     public Long getId() {
